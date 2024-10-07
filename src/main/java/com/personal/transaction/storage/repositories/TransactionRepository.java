5eb78ba159be1b1024929b2a78f8ef70a3c8c54f
jpsoubihe <jp.soubihe@gmail.com>
2024-10-06 22:08:12 -0300

    Add structure for DB interaction

---
+++ src/main/java/com/personal/transaction/storage/repositories/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.personal.transaction.storage.repositories;
+
+import com.personal.transaction.storage.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, String> {
+}
