4a9c7ed2109f1e0496bd50551aed536c0c2b3e9a
jpsoubihe <jp.soubihe@gmail.com>
2024-10-09 01:15:39 -0300

    Add repeatable migration for tests

---
+++ src/main/resources/db/migration/R__Populate_transactions.sql
@@ -0,0 +1,7 @@
+INSERT INTO transaction(transaction_id, description, amount, transaction_date)
+VALUES
+('d27d2481-94c6-41e7-9ba0-6caddeeec470','Test descritpion - 1', 23.45,1728440865),
+('31963a93-79c4-4279-add3-7234eb6ff5d7','Test descritpion - 2', 100.50,1728440865),
+('63b53f93-cd07-4c2a-98fc-b6f1554afd31','Test descritpion - 3', 77.00,1728455265),
+('e6c345b5-62ef-4672-ae61-0aec4fbe65bb','Test descritpion - 4', 12.30,1728440915),
+('bcaadd7c-45fc-4641-83ff-5d3baa202e20','Test descritpion - 5', 232.25,1728441915);
